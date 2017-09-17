db82c9ed7d1b4a0d828be177684565bc513273c4
alexthe31 <31119670+alexthe31@users.noreply.github.com>
2017-09-17 15:56:20 -0700

    Add files via upload

---
+++ src/horoscope.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class horoscope {
+public static void main(String[] args) {
+	String a = JOptionPane.showInputDialog("what");
+	if(a.equals("leo")) {
+		JOptionPane.showMessageDialog(null, "yeah boi");
+	
+	}
+	else
+	{
+		JOptionPane.showMessageDialog(null, "fool");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+}
